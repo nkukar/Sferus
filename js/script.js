bfe898944bb47cbff972b7a6408eb096b59cdef8
nkukar <nkukar@inbox.ru>
2022-02-10 02:50:39 +0300

    CON-62 add new style css cases block

--- js/script.js
+++ js/script.js
@@ -19,11 +19,6 @@ modalBtns.forEach(item => {
 })
 
 
-
-
-
-
-
 // Слайдер в  products
 
 const sliderBtns = document.querySelectorAll('.products-btn'),
@@ -54,73 +49,6 @@ sliderBtns.forEach ((item,index) => {
 	})
 })
 
-// Работа стрелок
-// if(screen.width < 1020) {
-// 	const arrowLeft = document.querySelector('.arrow-left'),
-// 	arrowRight = document.querySelector('.arrow-right'),
-// 	arrowAll = document.querySelectorAll('.products-btn--arrow');
-// 	let count = 0;
-
-// 	if(count == 0) {
-// 		arrowLeft.style.display = 'none';
-// 	}
-// 	function hideArr () {
-// 		sliderBtns.forEach(item => {
-// 			item.style.display = 'none';
-
-// 		})
-// 		sliderContents.forEach(items => {
-// 			items.style.display = 'none';
-// 		})
-
-// 	};
-// 	function showContentArr (i = 0) {
-// 		sliderContents[i].style.display = 'block';
-// 		sliderBtns[i].style.display = 'block';
-		
-// 	}
-// 	hideArr ();
-// 	showContentArr(0)
-
-// 	arrowAll.forEach(item => {
-// 		item.addEventListener('click', (e) => {
-// 			hideArr ();
-// 			if(e.target.classList.contains('arrow-right')) {
-// 				count++;
-// 				showContentArr(count)
-// 				arrowAll.forEach(arrow => {
-// 					arrow.style.display = 'block';
-// 				})
-// 				if(count == (sliderContents.length -1)) {
-// 					arrowRight.style.display = 'none';
-// 				}
-// 			}
-// 			if(e.target.classList.contains('arrow-left')) {
-// 				count--;
-// 				showContentArr(count)
-// 				arrowAll.forEach(arrow => {
-// 					arrow.style.display = 'block';
-// 				})
-// 				if(count == 0) {
-// 					arrowLeft.style.display = 'none';
-// 				}
-// 			}
-			
-// 		})
-// 	})
-// }
-
-
-
-
-// Отступ блока 
-
-	// const blockMargin1 = window.screen.availWidth;
-	// const sliderMargin1 = document.querySelector('.slider__items');
-	// sliderMargin1.style.width = blockMargin1+'px';
-
-	// const blockMargin = document.querySelector('.about__items').offsetLeft;
-	// sliderMargin1.style.paddingLeft = blockMargin+'px';
 
 
 
@@ -201,6 +129,45 @@ formBtn.addEventListener('click', (e) => {
  
 
 })
+
+
+
+// Вывод текста из FireBase
+
+// function showTextFireBase (block, nameBlock, element) {
+// 	for(let itemName in block) {
+// 		if(itemName == 'title') {
+// 			document.getElementById(`${nameBlock + '-title'}`).innerHTML = block[itemName];
+// 		}
+// 		if(itemName == 'subtitle') {
+// 			document.getElementById(`${nameBlock + '-subtitle'}`).innerHTML = block[itemName];
+// 		}
+// 		if(itemName == 'text') {
+// 			document.getElementById(`${nameBlock + '-text'}`).innerHTML = block[itemName];
+// 		}
+// 		if(itemName == 'items') {
+// 			let itemsListOut = '';
+// 			document.getElementById(`${nameBlock + '-items'}`).innerHTML = block[itemName];
+// 		}
+// 	}
+// }
+
+
+
+
+// if(window.location.pathname == '/index-ru.html') {
+// 	const getFireBase = firebase.database().ref('ru').on('value', (elem)=> {
+// 		const allInfo = elem.val();
+// 		for (let elem in allInfo) {
+// 			if(elem == 'main') {
+// 				const nameBlock = elem;
+// 				const getBlock = allInfo[elem];
+				
+// 			}
+// 		}
+// 	})
+// }
+
 
 
 
